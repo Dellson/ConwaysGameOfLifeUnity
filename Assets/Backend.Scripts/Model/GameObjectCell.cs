6aa3c1f6d0b737ee44d7224e050701ad1b5babfd
Paweł Dela <pawel.dela@amberteam.pl>
2021-04-25 18:45:20 +0200

    Performance improvement

--- Assets/Backend.Scripts/Model/GameObjectCell.cs
+++ Assets/Backend.Scripts/Model/GameObjectCell.cs
@@ -7,6 +7,7 @@ namespace ConwaysGameOfLife.Assets.Backend.Scripts.Model
         public Cell Cell { get; private set; }
         private Vector3 Coordinates;
         private readonly GameObject AssociatedGameObject;
+        private readonly SpriteRenderer SpriteRenderer;
         private const int CellPixelSize = 8;
 
         public GameObjectCell((int x, int y) coords, bool state, GameObject tileTemplate, Transform transformToAttachTo) 
@@ -15,12 +16,15 @@ namespace ConwaysGameOfLife.Assets.Backend.Scripts.Model
         public GameObjectCell(Cell cell, GameObject tileTemplate, Transform transformToAttachTo)
         {
             Cell = cell;
-            Coordinates = new Vector3(Cell.X * CellPixelSize / 2 - 500, Cell.Y * CellPixelSize / 2 - 500, 1);
+            Coordinates = new Vector3(Cell.X * CellPixelSize / 2 - 900, Cell.Y * CellPixelSize / 2 - 500, 1); // TODO get rid of const 900 and 500 values
 
             AssociatedGameObject = Instantiate(tileTemplate, Coordinates, Quaternion.identity);
             AssociatedGameObject.name = $"Cell({Cell.X})-({Cell.Y})";
 
+            SpriteRenderer = AssociatedGameObject.transform.Find("ImageHolder").gameObject.GetComponent<SpriteRenderer>();
+            SpriteRenderer.sprite = Resources.Load<Sprite>($"Sprites/true");
             UpdateStateImage();
+
             AssociatedGameObject.transform.SetParent(transformToAttachTo, false);
         }
 
@@ -28,15 +32,9 @@ namespace ConwaysGameOfLife.Assets.Backend.Scripts.Model
         {
             if (Cell.State != Cell.PreviousState)
             {
-                SpriteRenderer.sprite = Resources.Load<Sprite>($"Sprites/{Cell.State}");
+                SpriteRenderer.enabled = Cell.State;
                 Cell.PreviousState = Cell.State;
             }
-        }
-
-        /// <summary>
-        /// "ImageHolder" is a prefab's child GameObject holding SpriteRenderer
-        /// </summary>
-        private SpriteRenderer SpriteRenderer => 
-            AssociatedGameObject.transform.Find("ImageHolder").gameObject.GetComponent<SpriteRenderer>();
+        }            
     }
 }
